b62807a1e37ff32345797ce562e1ae20f9d99169
Ricky <148996006+Rickypatel777@users.noreply.github.com>
2023-11-28 23:42:54 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,14 @@
+let arr = [1, 2 , 4 , 5, 7]
+//         0, 1 , 2 , 3, 4
+
+/* arr[0] = 100;
+console.log(arr);
+console.log(arr.length)
+console.log(arr[0])
+console.log(arr[2])
+console.log(arr[4])
+*/
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+
---
+++ mfr.js
@@ -0,0 +1,30 @@
+// let arr = [1,2,3,4,5];
+// let newArr = []
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     newArr.push(element**10)
+// }
+
+// --------------------------------------
+
+// let newArr = arr.map((e)=>{
+//     return e**2
+// })
+// console.log(newArr)
+// const one = (e)=>{
+//     if(e>7){
+//         return true
+//     }
+//     return false
+// }
+// console.log(newArr.filter(one))
+
+// ---------------------------------------
+
+// let arr2 = [1,2,3,4,5]
+
+// const red = (a ,b)=>{
+//     return a*b
+// }
+
+// console.log(arr2.reduce(red))
